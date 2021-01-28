46a617c4e1e3681407c06b1d580ad5a6b68d8f89
txxgxxx <68065662+txxgxxx@users.noreply.github.com>
2021-01-28 22:25:17 +0900

    리스트를 추가하고 삭제하는 코드

---
+++ Day Two Blueprint.py
@@ -0,0 +1,35 @@
+"""
+As you can see, the code is broken.
+Create the missing functions, use default arguments.
+Sometimes you have to use 'return' and sometimes you dont.
+Start by creating the functions
+"""
+def is_on_list(list, day):
+  return day in list
+  
+  
+def get_x(list, number):
+  return list[number]
+
+def add_x(list, item_in_list):
+  list.append(item_in_list)
+
+def remove_x(list, item_in_list):
+  list.remove(item_in_list)
+# \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
+
+print("The fourth item in 'days' is:", get_x(days, 3))
+
+add_x(days, "Sat")
+print(days)
+
+remove_x(days, "Mon")
+print(days)
+
+
+# /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
+
